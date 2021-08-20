b3f7276a0175e8adf371f8789b8582dbec1ff85f
Space Time <Zeus6_6@163.com>
2021-08-20 10:20:03 +0800

    添加项目文件

---
+++ Vizpower Plugin Installer (WPF)/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Vizpower_Plugin_Installer_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
