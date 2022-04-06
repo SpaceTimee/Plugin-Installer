63fdf8bb95ab00dc2ea9eb1815459149a66d7477
Space Time <Zeus6_6@163.com>
2022-04-06 11:20:47 +0800

    4.4.1.4 更新

--- Vizpower Plugin Installer/MainWindow.xaml.cs
+++ Vizpower Plugin Installer/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.VisualBasic;
-using Microsoft.VisualBasic.CompilerServices;
 using MessageBox = System.Windows.Forms.MessageBox;
 using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
 
@@ -26,19 +23,16 @@ namespace Vizpower_Plugin_Installer
         //CaptureDesktop.dll和WxbPluginGUI.exe在Resources文件夹，替换掉原来的再编译即可
         //OriginalCaptureDesktop.dll是原版无限宝CaptureDesktop.dll，用于在拆卸时还原，如文件有更新，可将文件名改为OriginalCaptureDesktop.dll并替换
 
-        private const string SpecialVersion = "";    //特殊版本后缀
+        private const string SpecialVersionSuffix = "";    //特殊版本后缀
         private const bool SkipUpdate = false;  //是否跳过开启时的检查更新
         private const bool KillWxbBeforeInstall = true; //安装前是否自动杀死无限宝相关进程
         private const bool KillWxbBeforeUninstall = true;   //拆卸前是否自动杀死无限宝相关进程
         private const string AgreementUrl = @"https://yuhuison-1259460701.cos.ap-chengdu.myqcloud.com/mzsm.html";   //用户协议Url
         private const string InstallTipUrl = @"https://gitee.com/klxn/wxbplugin/raw/master/install.png";    //安装提示Url
-        private const string InstallVideoUrl = @"https://www.bilibili.com/video/BV1Ca4y1E7mx";  //使用教程Url
+        private const string InstallVideoUrl = @"https://www.bilibili.com/video/BV1Ca4y1E7mx";  //视频教程Url
 
         //注意:打包请勿修改下方内容
-        private static readonly Version CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version; //当前版本
-        private static readonly int CurrentVersionCode = int.Parse(CurrentVersion.Major.ToString() + CurrentVersion.Minor.ToString() + CurrentVersion.Build.ToString());  //当前版本号
-
-        private static string FileName = "";   //LoginTool.exe的名字
+        private static readonly Version CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version; //当前版本信息
         private static string CaptureDesktopPath = "";
         private static string WxbPluginGUIExePath = "";
         private static string WxbPluginGUIDllPath = "";
@@ -48,75 +42,52 @@ namespace Vizpower_Plugin_Installer
             InitializeComponent();
 
             //联网检查更新
-            if (!SkipUpdate && Strings.Left(Environment.OSVersion.ToString(), 22) != "Microsoft Windows NT 5")
+            if (!SkipUpdate)
                 Task.Run(CheckUpdateOnline);
 
             //修改全局标题
-            Title = "无限宝第三方插件 " + CurrentVersion.Major + "." + CurrentVersion.Minor + "." + CurrentVersion.Build + " " + SpecialVersion + (string.IsNullOrEmpty(SpecialVersion) ? "安装器" : " 安装器");
+            Title = "无限宝第三方插件 " + CurrentVersion.ToString().Substring(0, 5) + " " + SpecialVersionSuffix + (string.IsNullOrEmpty(SpecialVersionSuffix) ? "安装器" : " 安装器");
 
             //填充安装路径
-            if (args.Length >= 1 && File.Exists(args[0]) && Path.GetFileName(args[0]).StartsWith("LoginTool") && Path.GetFileName(args[0]).EndsWith(".exe"))
-                DealWithPath(args[0]);
-            else if (File.Exists(Properties.Settings.Default.FilePath))
-                DealWithPath(Properties.Settings.Default.FilePath);
+            if (args.Length >= 1)
+                PathTextBox.Text = args[0];
+            else
+                PathTextBox.Text = Properties.Settings.Default.FilePath;
         }
-        private void CheckUpdateOnline()
+        private async void CheckUpdateOnline()
         {
             try
             {
-                string WebText = GetWebCode("https://gitee.com/klxn/wxbplugin/raw/master/service.txt"); //API返回结果
+                string currentVersionCode = CurrentVersion.Major.ToString() + CurrentVersion.Minor.ToString() + CurrentVersion.Build.ToString();  //当前版本号
+                string httpResponseStr = await new HttpClient().GetStringAsync("https://gitee.com/klxn/wxbplugin/raw/master/service.txt");
+                string latestVersion = Strings.Split(httpResponseStr, "<版本>")[1];
+                string latestVersionCode = "";
+                for (int i = 1; i <= latestVersion.Length; ++i)
+                    latestVersionCode += (Strings.AscW(Strings.Mid(latestVersion, i, 1)) >= 48 && Strings.AscW(Strings.Mid(latestVersion, i, 1)) <= 57) ? Strings.Mid(latestVersion, i, 1) : "";
 
-                if (Strings.Split(WebText, "<版本>").ToList().Count < 3)
+                if (int.Parse(latestVersionCode) > int.Parse(currentVersionCode))
                 {
-                    Dispatcher.Invoke(new Action(() =>
-                    {
-                        MessageBox.Show("检查更新失败，请检查网络连接", Title);
-                    }));
-                    return;
-                }
+                    string forceUpdate = Strings.Split(httpResponseStr, "<强制更新>")[1];
+                    string downLoadURL = Strings.Split(httpResponseStr, "<链接>")[1];
 
-                string LatestVersion = Strings.Split(WebText, "<版本>")[1];
-
-                int LatestVersionCode = 0;
-                try
-                {
-                    string Num = "";
-                    for (int i = 1; i <= LatestVersion.Length; ++i)
-                        Num += (Strings.AscW(Strings.Mid(LatestVersion, i, 1)) >= 48 && Strings.AscW(Strings.Mid(LatestVersion, i, 1)) <= 57) ? Strings.Mid(LatestVersion, i, 1) : "";
-
-                    LatestVersionCode = int.Parse(Num);
-                }
-                catch
-                {
-                    Dispatcher.Invoke(new Action(() =>
-                    {
-                        MessageBox.Show("处理联网信息时发生错误，请向开发者反馈", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }));
-                    return;
-                }
-                if (LatestVersionCode > CurrentVersionCode)
-                {
-                    string ForceUpdate = Strings.Split(WebText, "<强制更新>")[1];
-                    string DownLoadURL = Strings.Split(WebText, "<链接>")[1];
-
-                    if (ForceUpdate == "0")
+                    if (forceUpdate == "0")
                     {
                         //非强制更新
-                        Dispatcher.Invoke(new Action(() =>
+                        Dispatcher.Invoke(() =>
                         {
-                            if (MessageBox.Show("插件已有更新，最新版本：" + LatestVersion + "\n是否跳转下载更新？", Title, MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
-                                Process.Start(DownLoadURL);
-                        }));
+                            if (MessageBox.Show("插件已有更新，最新版本：" + latestVersion + "\n是否跳转下载更新？", Title, MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                                Process.Start(downLoadURL);
+                        });
                     }
                     else
                     {
                         //强制更新
-                        Dispatcher.Invoke(new Action(() =>
+                        Dispatcher.Invoke(() =>
                         {
-                            MessageBox.Show("本版本已停用，最新版本：" + LatestVersion + "\n即将跳转下载更新", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }));
+                            MessageBox.Show("本版本已停用，最新版本：" + latestVersion + "\n即将跳转下载更新", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        });
 
-                        Process.Start(DownLoadURL);
+                        Process.Start(downLoadURL);
 
                         Environment.Exit(0);
                     }
@@ -124,140 +95,52 @@ namespace Vizpower_Plugin_Installer
             }
             catch
             {
-                Dispatcher.Invoke(new Action(() =>
-                {
-                    MessageBox.Show("处理联网信息时发生错误，请向开发者反馈", Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }));
+                Dispatcher.Invoke(() => { MessageBox.Show("检查更新时发生错误，请检查网络连接", Title, MessageBoxButtons.OK, MessageBoxIcon.Error); });
                 return;
             }
         }
-        private string GetWebCode(string strURL)
+
+        //路径处理
+        private void PathTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            Uri arg_15_0 = new Uri(strURL);
-            byte[] i;
-            Queue<byte> dataQue = new Queue<byte>();
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(arg_15_0);
-            DateTime sTime = Conversions.ToDate("1990-09-21 00:00:00");
-            httpReq.IfModifiedSince = sTime;
-            httpReq.Method = "GET";
-            httpReq.Timeout = 6000;
-            HttpWebResponse httpResp;
-            try
+            if (File.Exists(PathTextBox.Text) && Path.GetFileName(PathTextBox.Text).StartsWith("LoginTool") && Path.GetFileName(PathTextBox.Text).EndsWith(".exe"))
             {
-                httpResp = (HttpWebResponse)httpReq.GetResponse();
+                PathTextBox.Foreground = Brushes.Black;
+                DealWithPath(PathTextBox.Text);
             }
-            catch (Exception arg_58_0)
-            {
-                ProjectData.SetProjectError(arg_58_0);
-                string GetWebCode = "<title>no thing found</title>";
-                ProjectData.ClearProjectError();
-                return GetWebCode;
-            }
-            Stream ioS = httpResp.GetResponseStream();
-            checked
-            {
-                while (ioS.CanRead)
-                {
-                    try
-                    {
-                        dataQue.Enqueue((byte)ioS.ReadByte());
-                    }
-                    catch (Exception arg_87_0)
-                    {
-                        ProjectData.SetProjectError(arg_87_0);
-                        ProjectData.ClearProjectError();
-                        break;
-                    }
-                }
-                i = new byte[dataQue.Count - 1 + 1];
-                int num = dataQue.Count - 1;
-                for (int j = 0; j <= num; j++)
-                {
-                    i[j] = dataQue.Dequeue();
-                }
-                string tCode = Encoding.GetEncoding("UTF-8").GetString(i);
-                string charSet = Strings.Replace(Conversions.ToString(GetByDiv2(tCode, "charset=", "\"")), "\"", "", 1, -1, CompareMethod.Binary);
-                if (Operators.CompareString(charSet, "", false) == 0)
-                {
-                    if (Operators.CompareString(httpResp.CharacterSet, "", false) == 0)
-                    {
-                        tCode = Encoding.GetEncoding("UTF-8").GetString(i);
-                    }
-                    else
-                    {
-                        tCode = Encoding.GetEncoding(httpResp.CharacterSet).GetString(i);
-                    }
-                }
-                else
-                {
-                    tCode = Encoding.GetEncoding(charSet).GetString(i);
-                }
-                string GetWebCode = tCode;
-                if (Operators.CompareString(tCode, "", false) == 0)
-                {
-                    GetWebCode = "<title>no thing found</title>";
-                }
-                return GetWebCode;
-            }
+            else
+                PathTextBox.Foreground = Brushes.Red;
         }
-        private object GetByDiv2(string code, string divBegin, string divEnd)
+        private void DealWithPath(string filePath)
         {
-            int lens = Strings.Len(divBegin);
-            checked
-            {
-                object GetByDiv2;
-                if (Strings.InStr(1, code, divBegin, CompareMethod.Binary) == 0)
-                {
-                    GetByDiv2 = "";
-                }
-                else
-                {
-                    int lgStart = Strings.InStr(1, code, divBegin, CompareMethod.Binary) + lens;
-                    int lgEnd = Strings.InStr(lgStart + 1, code, divEnd, CompareMethod.Binary);
-                    if (lgEnd == 0)
-                    {
-                        GetByDiv2 = "";
-                    }
-                    else
-                    {
-                        GetByDiv2 = Strings.Mid(code, lgStart, lgEnd - lgStart);
-                    }
-                }
-                return GetByDiv2;
-            }
+            string fileName = Path.GetFileName(filePath);    //文件名(包括后缀名)
+
+            CaptureDesktopPath = filePath.Replace(fileName, "CaptureDesktop.dll");
+            WxbPluginGUIExePath = filePath.Replace(fileName, "WxbPluginGUI.exe");
+            WxbPluginGUIDllPath = filePath.Replace(fileName, "wxbPluginGUI.dll");
+
+            Properties.Settings.Default.FilePath = filePath;
+            Properties.Settings.Default.Save();
+
+            if (File.Exists(CaptureDesktopPath) && File.Exists(WxbPluginGUIExePath))
+                InstallButton.Content = "更新";
         }
 
-        //安装路径
+        //浏览路径
         private void NavigateButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog { Filter = "无限宝登陆工具|LoginTool*.exe" };
 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                DealWithPath(openFileDialog.FileName);
-        }
-        private void DealWithPath(string filePath)
-        {
-            LocationTextBox.Text = filePath;    //路径
-            FileName = Path.GetFileName(filePath);    //文件名(包括后缀名)
-
-            CaptureDesktopPath = LocationTextBox.Text.Replace(FileName, "CaptureDesktop.dll");
-            WxbPluginGUIExePath = LocationTextBox.Text.Replace(FileName, "WxbPluginGUI.exe");
-            WxbPluginGUIDllPath = LocationTextBox.Text.Replace(FileName, "wxbPluginGUI.dll");
-
-            Properties.Settings.Default.FilePath = LocationTextBox.Text;
-            Properties.Settings.Default.Save();
-
-            if (File.Exists(CaptureDesktopPath) &&
-                File.Exists(WxbPluginGUIExePath))
-                InstallButton.Content = "更新";
+                PathTextBox.Text = openFileDialog.FileName;
         }
 
         //安装拆卸
         private void InstallButton_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(LocationTextBox.Text) || string.IsNullOrEmpty(FileName))
+            if (PathTextBox.Foreground != Brushes.Black)
             {
-                MessageBox.Show("请点击浏览找到 LoginTool.exe 文件", Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("请在输入框中填入正确的 LoginTool.exe 文件路径", Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
 
@@ -296,9 +179,9 @@ namespace Vizpower_Plugin_Installer
         }
         private void UninstallButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(LocationTextBox.Text) || string.IsNullOrEmpty(FileName))
+            if (PathTextBox.Foreground != Brushes.Black)
             {
-                MessageBox.Show("请点击浏览找到 LoginTool.exe 文件", Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("请在输入框中填入正确的 LoginTool.exe 文件路径", Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
 
